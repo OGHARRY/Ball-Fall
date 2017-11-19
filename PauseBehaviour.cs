1cc396c3bbc1a257eaaee23b454139cccdfb1c3a
OGHARRY <33790468+OGHARRY@users.noreply.github.com>
2017-11-19 01:59:13 -0700

    PauseButton

---
+++ PauseBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PauseBehaviour : MonoBehaviour {
+
+    public bool paused;
+    // Use this for initialization
+    void Start() {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Pause() {
+
+        if (paused) {
+            Time.timeScale = 0;
+        } else if (!paused) {
+            Time.timeScale = 1;
+        }
+}
+}
